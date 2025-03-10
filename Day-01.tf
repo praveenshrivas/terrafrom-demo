cdb629f463bd21c471ed99314cde0b7debc9d627
praveenshrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-03-10 21:09:50 +0530

    Update Day-01.tf

--- Day-01.tf
+++ Day-01.tf
@@ -10,3 +10,21 @@ resource "aws_instance" "example" {
 output "public_ip" {
   value = aws_instance.example.public_ip
 }
+
+
+"""
+When & Where Can You Set the Variable Name?
+1. Using a Terraform CLI Argument (-var)
+terraform apply -var "instance_type=t3.micro"
+
+2. Using a .tfvars File
+Create a file terraform.tfvars:
+instance_type = "t3.micro"
+
+3. Using Environment Variables (TF_VAR_)
+Set the variable in your terminal:
+
+export TF_VAR_instance_type="t3.micro"
+terraform apply
+
+"""
